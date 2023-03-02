0d1cb6be2e54331dfa434f7f3871a41214b12fd1
Liuemith Sánchez <114295544+Liuemith@users.noreply.github.com>
2023-03-02 11:23:02 +0100

    auth.js Actualizacion
     codigo comentado (verificar sintaxis) 
    
    -useNavigate

---
+++ origen/componentes/Auth.js
@@ -0,0 +1,49 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../context/AuthContext";
+
+export const Auth = () => {
+  const { user, logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  return (
+    user && (
+      <p>
+        Usuario:<Link to={`/user`}>{user.nombre} </Link> registrado
+        <button onClick={() => logOut() && navigate("/login")}>Salir</button>
+      </p>
+    )
+  );
+};
+
+
+/*
+import "../css/authPage.css";
+import iconSalir from "../images/iconSalir.png";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../context/AuthContext";
+
+export const Auth = () => {
+  const { user, logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  return (
+    user && (
+      <>
+        <Link className="enlaceMiPerfil" to={`/user/${user.id}`}>
+          Mi Perfil
+          {/* {user.nombre}{" "} */}
+       /* </Link>
+
+        <button className="botonSalir" onClick={() => logOut()}>
+          Salir
+          <img className="iconoSalir" src={iconSalir}></img>
+        </button>
+      </>
+    )
+  );
+};
+
+
+*/
